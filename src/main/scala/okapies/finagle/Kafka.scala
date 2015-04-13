f19a6a1bec09d906d3c82aff60251bd41eb1e0b2
Mikael Högqvist <mikael@peerialism.com>
2015-04-13 20:37:36 +0200

    Client is created as a stacked client according to the new finagle API

--- src/main/scala/okapies/finagle/Kafka.scala
+++ src/main/scala/okapies/finagle/Kafka.scala
@@ -1,8 +1,9 @@
 package okapies.finagle
 
-import com.twitter.finagle.{Client, Name}
-import com.twitter.finagle.client.{Bridge, DefaultClient}
+import com.twitter.finagle.{Client, Name, Stack, Service, ServiceFactory, param}
+import com.twitter.finagle.client.{Bridge, DefaultClient, StackClient, StdStackClient, Transporter}
 import com.twitter.finagle.dispatch.PipeliningDispatcher
+import com.twitter.finagle.transport.Transport
 import com.twitter.finagle.netty3.Netty3Transporter
 import com.twitter.finagle.pool.SingletonPool
 import com.twitter.finagle.stats.StatsReceiver
@@ -17,20 +18,43 @@ trait KafkaRichClient { self: Client[Request, Response] =>
 
 }
 
-object KafkaTransporter extends Netty3Transporter[Request, Response](
-  name = "kafka",
-  pipelineFactory = KafkaBatchClientPipelineFactory
-)
-
-object KafkaClient extends DefaultClient[Request, Response](
-  name = "kafka",
-  endpointer =
-    Bridge[Request, Response, Request, Response](KafkaTransporter, new PipeliningDispatcher(_)),
-  pool = (sr: StatsReceiver) => new SingletonPool(_, sr)
-) with KafkaRichClient
-
 object Kafka extends Client[Request, Response] with KafkaRichClient {
 
-  def newClient(dest: Name, label: String) = KafkaClient.newClient(dest, label)
 
+  object Client {
+    val stack: Stack[ServiceFactory[Request, Response]] = StackClient.newStack
+
+//      .replace(StackClient.Role.pool, new SingletonPool.module[Request, Response])
+  }
+
+  case class Client(
+    stack: Stack[ServiceFactory[Request, Response]] = Client.stack,
+    params: Stack.Params = StackClient.defaultParams
+  ) extends StdStackClient[Request, Response, Client] {
+
+    protected type In = Request
+    protected type Out = Response
+
+    protected def copy1(
+      stack: Stack[ServiceFactory[Request, Response]] = this.stack,
+      params: Stack.Params = this.params
+    ) = copy(stack, params)
+
+    protected def newTransporter(): Transporter[In, Out] = {
+      Netty3Transporter(KafkaBatchClientPipelineFactory, params)
+    }
+
+    protected def newDispatcher(
+      transport: Transport[Request, Response]
+    ): Service[Request, Response] = {
+      val param.Stats(sr) = params[param.Stats]
+      val param.Label(name) = params[param.Label]
+      new PipeliningDispatcher(transport)
+    }
+
+  }
+
+  val client = Client()
+
+  def newClient(dest: Name, label: String) = client.newClient(dest, label)
 }
